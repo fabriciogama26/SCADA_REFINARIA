242edb81646babf85c6c59243725a7ecb0363b61
fabriciogama26 <toka.fgg@gmail.com>
2022-12-14 14:17:08 -0300

    atualizado

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from mainwidget import Mainwidget 
 from kivy.lang.builder import Builder
 
 
-class MainApp(App):
+class MainApp(MDApp):
     """
     classe com o aplicativo
     """
@@ -11,9 +11,10 @@ class MainApp(App):
         """
         Método que gera o aplicativo com no widget principal
         """
-        self. _widget = Mainwidget()
+        self. _widget = Mainwidget(scan_time = 1000, server_ip="127.0.0.1", server_port=502)
         return self._widget
     
 if __name__ =='__main__':
     Builder.load_string(open("mainwidget.kv", encoding = "utf-8").read(), rulesonly = True)
+    Builder.load_string(open("popups.kv", encoding = "utf-8").read(), rulesonly = True)
     MainApp().run()

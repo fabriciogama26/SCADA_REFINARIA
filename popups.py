98df7580d22dcdd0cfa715779403b8c2a1f47f07
fabriciogama26 <toka.fgg@gmail.com>
2022-12-15 10:35:05 -0300

    add grafico de linha em tempo real

--- popups.py
+++ popups.py
@@ -39,8 +39,8 @@ class DataGraphPopup(Popup):
     '''
     Popup do gráfico do fornalha
     '''
-    def _init_(self,xmax, plot_color, **kwargs): 
-        super()._init_(*kwargs)
+    def __init__(self, xmax, plot_color, **kwargs): 
+        super().__init__(**kwargs)
         self.plot = LinePlot(line_width = 1.5, color = plot_color)
         self.ids.graph.add_plot(self.plot) 
         self.ids.graph.xmax = xmax
